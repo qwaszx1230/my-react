04bb1b1beb7eb93128371c9e50382963972cfd56
holly.zhang <holly.zhang@akmii.com>
2021-09-28 10:17:53 +0800

    update typescript

--- src/component/typescript.tsx
+++ src/component/typescript.tsx
@@ -1,6 +1,6 @@
 import { Button } from 'antd';
 import * as React from 'react';
-import { Student } from "../util/greeter";
+import { AnalogClock, cerateClock, Clock, DigitalClock, Control, SelectControl, Student, TextBox } from "../util/greeter";
 
 interface SquareConfig {
     color?: string;
@@ -29,6 +29,73 @@ export class TypescriptPage extends React.Component<any, any>{
     stadent() {
         let user = new Student("Jane", "M.", "User");
         console.log(user);
+        let clock = new Clock(15, 20);
+        console.log(clock);
+        clock.setTime(new Date().toString());
+        console.log(clock);
+        let digital = cerateClock(DigitalClock, 15, 20);
+        let analog = cerateClock(AnalogClock, 14, 20);
+        digital.tick();
+        analog.tick();
+        console.log(digital);
+        console.log(analog);
+
+        const control: TextBox = null;
+
+    }
+
+    //类 继承
+    extendsClass() {
+        class Animal {
+            name: string;
+            constructor(theName: string) {
+                this.name = theName;
+            }
+            move(distanceInMeters: number = 0) {
+                console.log(`${this.name} moved ${distanceInMeters}`);
+            }
+        }
+
+        class Snake extends Animal {
+            constructor(name: string) {
+                super(name); //此时的super 当作函数使用，this指向Snake
+            }
+            move(distanceInMeters: number = 5) {
+                console.log("Slithering...");
+                super.move(distanceInMeters);//这里的super 被当作对象使用，在普通方法中，指向父类的原型对象；在静态方法中，指向父类 this指向Animal
+            }
+        }
+
+        class Horse extends Animal {
+            constructor(name: string) {
+                super(name);
+            }
+            move(distanceInMeters: number = 45) {
+                console.log("Galloping...");
+                super.move(distanceInMeters);
+            }
+        }
+
+        class Orange {
+            name: string;
+            constructor() {
+                this.name = "Fruits";
+            }
+        }
+
+        class Apple extends Animal {
+            constructor(name: string) {
+                super(name);
+                this.name = "test";
+                super.name = "Bob";
+            }
+
+        }
+
+        let sam = new Snake("Sammy the phthon");
+        let tom: Animal = new Horse("Tommy the Palomino");
+        sam.move();
+        tom.move();
     }
 
 
@@ -37,6 +104,7 @@ export class TypescriptPage extends React.Component<any, any>{
             <h2 className="ts-title">开始学习Typescript</h2>
 
             <Button onClick={() => this.stadent()}>学习类</Button>
+            <Button onClick={() => this.extendsClass()}>继承类</Button>
         </div>
     }
 
--- src/util/greeter.ts
+++ src/util/greeter.ts
@@ -1,7 +1,104 @@
+//implements是一个类实现一个接口用的关键字.实现一个接口，必须实现接口中的所有方法。
+
 //类  在构造函数的参数上使用public等同于创建了同名的成员变量。
-export class Student  {
+export class Student {
     fullName: string;
     constructor(public firstName: string, public middleName: string, public lastName: string) {
         this.fullName = firstName + " " + middleName + " " + lastName;
     }
+}
+//可索引类型(只支持数字和字符串)
+interface StringArray {
+    [index: number]: string;
+}
+let myArray: StringArray = ["aa", "bb"];
+class Animal {
+    name: string;
+}
+class Dog extends Animal {
+    breed: string;
+}
+
+
+interface NotOkay {
+    [x: number]: Animal;
+    // [x: string]: Dog;// 错误：使用数值型的字符串索引，有时会得到完全不同的Animal!
+}
+
+//索引签名设置只读
+interface ReadonlyString {
+    readonly [index: string]: string;
+}
+
+//类 类型
+interface ClockInterface {
+    currtentTime: string;
+}
+
+export class Clock implements ClockInterface {
+    currtentTime: string;
+    setTime(d: string) {
+        this.currtentTime = d;
+    }
+    constructor(h: number, m: number) {
+        this.currtentTime = `${h}:${m}`
+    }
+}
+
+interface ClockInterface2 {
+    tick();
+}
+
+interface ClockConstructure {
+    new(h: number, m: number): ClockInterface2;
+}
+
+//错误的调用
+// class Clock2 implements ClockConstructure {
+//     currentTime: string;
+//     constructor(h: number, m: number) { }
+// }
+
+//constructor为静态函数，不在检查类型范围，createClock的第一个参数为构造函数，createClock(AnalogClock, 7, 32)里 会检查AnalogClock是个符合构造函数的类型签名
+export function cerateClock(ctor: ClockConstructure, h: number, m: number): ClockInterface2 {
+    return new ctor(h, m);
+}
+
+export class DigitalClock implements ClockInterface2 {
+    currtentTime: string;
+    constructor(h: number, m: number) {
+        this.currtentTime = `${h}:${m}`
+    }
+    tick() {
+        console.log("beep beep");
+    }
+}
+
+export class AnalogClock implements ClockInterface2 {
+    currtentTime: string;
+    constructor(h: number, m: number) {
+        this.currtentTime = `${h}:${m}`
+    }
+    tick() {
+        console.log("tick tock");
+    }
+}
+
+//接口继承类
+export class Control {
+    private state: string;
+}
+//SelectableControl包含了Control的所有成员
+export interface SelectControl extends Control {
+    select(): void;
+}
+
+export class TextBox extends Control {
+    select() { };
+}
+
+
+//在Control类内部，是允许通过SelectableControl的实例来访问私有成员state的
+export class Button extends Control implements SelectControl {
+    select() { };
 }
